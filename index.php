d22e535725ede418a5255bf4ae4dc3c0ea87f546
MatteoCarbone17 <matteocarbone17@live.com>
2023-01-25 15:08:40 +0100

    foreach in foreach to print element

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -47,13 +48,29 @@
         ],
     ];
 
-
     ?>
+
+    <header>
+        <h1>
+            Hotels 
+        </h1>
+    </header>
     <main>
+
         <pre>
          <?php var_dump($hotels) ?>
         </pre>
-
+        <ul>
+            <li>
+                <?php foreach($hotels as $value => $hotelList){
+                     foreach($hotelList as $element){
+                        echo  '  ' .  $element;
+                     }
+                     
+                }
+                ?>
+            </li>
+        </ul>
     </main>
 </body>
 
